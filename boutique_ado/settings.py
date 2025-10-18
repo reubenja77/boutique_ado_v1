7ff262d3a9562fa37f28f12fb5290e817ec2cd07
Reuben <reubenabrahams77@gmail.com>
2025-10-18 22:13:04 +0200

    Added mapping webhooks to custom handlers

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -36,7 +36,10 @@ SECRET_KEY = 'django-insecure-_l==4hu-y@(7b+up%p5$$)^g14j61)!^d2&^olfwm=hl!-@j(5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '127.0.0.1', # vs code preview
+    'localhost', # listen for stripe webhooks
+]
 
 
 # Application definition
@@ -183,6 +186,7 @@ STANDARD_DELIVERY_PERCENTAGE = 10
 STRIPE_CURRENCY = 'usd'
 STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
+STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
